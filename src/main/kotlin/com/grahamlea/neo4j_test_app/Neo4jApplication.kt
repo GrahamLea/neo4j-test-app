091d557db7d4619d4908e08c42d70c4d001ee0c0
Michael Simons <michael@simons.ac>
2020-03-06 13:38:33 +0100

    Remove redundant beans, they are automically created via our starter.

--- src/main/kotlin/com/grahamlea/neo4j_test_app/Neo4jApplication.kt
+++ src/main/kotlin/com/grahamlea/neo4j_test_app/Neo4jApplication.kt
@@ -2,14 +2,11 @@ package com.grahamlea.neo4j_test_app
 
 import com.grahamlea.neo4j_test_app.model.ThingRepository
 import org.neo4j.ogm.config.Configuration
-import org.neo4j.ogm.session.SessionFactory
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.context.ConfigurableApplicationContext
 import org.springframework.context.annotation.Bean
-import org.springframework.context.annotation.ComponentScan
 import org.springframework.data.neo4j.repository.config.EnableNeo4jRepositories
-import org.springframework.data.neo4j.transaction.Neo4jTransactionManager
 import kotlin.system.exitProcess
 
 @SpringBootApplication
@@ -20,11 +17,6 @@ class GraphApiApplication {
 	@Bean
 	fun configuration(): Configuration = Configuration.Builder().uri(neo4jUrl).build()
 
-	@Bean
-	fun sessionFactory() = SessionFactory(configuration(), "com.grahamlea.neo4j_test_app")
-
-	@Bean
-	fun transactionManager() = Neo4jTransactionManager(sessionFactory())
 }
 
 fun main(args: Array<String>) {
