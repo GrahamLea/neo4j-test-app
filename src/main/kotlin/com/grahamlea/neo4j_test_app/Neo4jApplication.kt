97a8ac1c1d0f41e99ca0f09322c033a50ad2cc34
Michael Simons <michael@simons.ac>
2020-03-06 13:38:59 +0100

    Remove superflous configuration, this is also done via the starter.
    Use the standard properties instead. Here I am implying an instance on
    localhost with the given credentials.

--- src/main/kotlin/com/grahamlea/neo4j_test_app/Neo4jApplication.kt
+++ src/main/kotlin/com/grahamlea/neo4j_test_app/Neo4jApplication.kt
@@ -1,22 +1,15 @@
 package com.grahamlea.neo4j_test_app
 
 import com.grahamlea.neo4j_test_app.model.ThingRepository
-import org.neo4j.ogm.config.Configuration
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.context.ConfigurableApplicationContext
-import org.springframework.context.annotation.Bean
 import org.springframework.data.neo4j.repository.config.EnableNeo4jRepositories
 import kotlin.system.exitProcess
 
 @SpringBootApplication
 @EnableNeo4jRepositories("com.grahamlea.neo4j_test_app.model")
 class GraphApiApplication {
-	private val neo4jUrl = "bolt://localhost:7687"
-
-	@Bean
-	fun configuration(): Configuration = Configuration.Builder().uri(neo4jUrl).build()
-
 }
 
 fun main(args: Array<String>) {
